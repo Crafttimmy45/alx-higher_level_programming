6ca7daff58f7aa596b58494b3260167559880ac4
Olonade OLuwatimilehin <olonadeoluwatimilehin@gmail.com>
2024-07-16 06:49:30 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""print the alphabet in lowercase, not followed by a new line."""
+
 for i in range(97, 123):
-    print("{:c}".format(i), end='')
+    print("{}".format(chr(i)), end='')
